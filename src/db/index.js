c08614d6274a82d653060d0642672e17b6d797bc
Mathius <devachievem@gmail.com>
2021-03-19 21:33:12 -0700

    Add database_url env variable

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,7 @@
 const pg = require("pg");
-console.log(process.env.DBHOST);
+
 const client = new pg.Client({
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
-  database: process.env.DB,
-  port: 5432,
-  host: process.env.DBHOST,
-  ssl: {rejectUnauthorized: false}
+  connectionString: process.env.DATABASE_URL || ""
 });
 
 client
