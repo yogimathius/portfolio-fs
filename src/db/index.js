6bdc0f8b1d04dc9d97af6c860340f5b1f5a486ac
Mathius <devachievem@gmail.com>
2021-03-19 21:13:14 -0700

    add host, password, db to .env for client conn

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,12 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  user: "ctnuuhzxxwhwsi",
+  process.env.DBPASSWORD: "6b99459ba180ac20eba82a79c9ddbd81e59b9e3a929dfde8dcbfded506130e51",
+  process.env.DB: "d45flg8j9d0dui",
+  port: 5432,
+  process.env.DBHOST: "ec2-18-204-101-137.compute-1.amazonaws.com",
+  ssl: {rejectUnauthorized: false}
 });
 
 client
