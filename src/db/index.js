4ff3041e63220ce2debf59f10928af6e30abd408
Mathius <devachievem@gmail.com>
2021-04-29 09:36:39 -0700

    reject unauthorzed set to false

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,13 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  connectionString: process.env.DATABASE_URL || "",
+  ssl: {
+    rejectUnauthorized: false
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 client
