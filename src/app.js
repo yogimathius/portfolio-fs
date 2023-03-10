b3732d176574de6ed8936736d09a28903addf3a0
Mathius <info@yogimathius.dev>
2023-03-09 22:18:05 -0800

    fix: add headers

--- src/app.js
+++ src/app.js
@@ -36,6 +36,13 @@ module.exports = function application(
   app.use(cors({
     origin: "https://www.yogimathius.dev"
   }));
+  app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
   app.use(helmet());
   app.use(bodyparser.json());
 
