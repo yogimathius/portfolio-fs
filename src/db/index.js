0429503dad72fe0b9a2fa95b0fc8c204c5950bdb
Mathius <info@yogimathius.dev>
2023-03-17 22:01:00 -0700

    fix: update db connect

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
-  tls: {
-    rejectUnauthorized: false
-  }
+  user: "hnciykslpjiycc",
+  password: "eed28cee8fc12fa9bf1b40b151c2b0482c3af8846794755f575d953829d02d4b",
+  database: "d3scjafqtb7gdj",
+  host: "ec2-3-93-160-246.compute-1.amazonaws.com",
+  ssl: {rejectUnauthorized: false}
 });
 
 client
