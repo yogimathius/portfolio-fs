a3b42ababa2a8c72a603cf5cd26dbc11a1ce96d3
Mathius <info@yogimathius.dev>
2023-03-20 09:19:54 -0700

    chore: update db creds

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,10 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  user: "hnciykslpjiycc",
-  password: "eed28cee8fc12fa9bf1b40b151c2b0482c3af8846794755f575d953829d02d4b",
-  database: "d3scjafqtb7gdj",
-  host: "ec2-3-93-160-246.compute-1.amazonaws.com",
+  user: "rpbknqnretiivu",
+  password: "72b7a432a18b760e3c378527edc173e913ca8bfb5b2e304021e01ac63e30d872",
+  database: "d78dek7upeoe0a",
+  host: "ec2-3-230-24-12.compute-1.amazonaws.com",
   ssl: {rejectUnauthorized: false}
 });
 
